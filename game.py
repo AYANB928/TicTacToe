366d7f327d43c274fe260c25cc70b65e14ba1550
AYANB928 <95986399+AYANB928@users.noreply.github.com>
2023-01-15 19:08:14 -0500

    Update game.py

--- game.py
+++ game.py
@@ -2,16 +2,22 @@ import tkinter as tk
 from tkinter import messagebox
 import random
 
-#create a Tkinter instance and setting winning combinations, buttons for game, player symbols, and title of game
+#create a Tkinter instance and setting winning combinations, buttons for game, player symbols, color, and title of game
 class TicTacToe:
     def __init__(self):
-        self.root = tk.Tk()
+        self.root = tk.Tk()  #create a Tkinter instance
         self.root.title("Tic-Tac-Toe")
         self.player = "X"
         self.computer = "O"
         self.buttons = [tk.Button(self.root, width=5, height=2, command=lambda x=i: self.play(x)) for i in range(9)]
         self.winning_combinations = [(0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6)]
+        self.color_var = tk.StringVar(value="Default")
+        self.color_var.trace("w", self.change_color)
         self.setup_gui()
+    
+    #Function changes the background color of the window
+    def change_color(self, *args):
+        self.root.config(bg=self.color_var.get())
 
     #setting up Tic Tac Toe Board and Play/Quit Buttons 
     def setup_gui(self):
@@ -22,6 +28,10 @@ class TicTacToe:
         self.play_again_button.grid(row=3, column=0, columnspan=3, pady=10)
         self.quit_button = tk.Button(self.root, text="Quit", command=self.root.destroy)
         self.quit_button.grid(row=4, column=0, columnspan=3)
+        
+        tk.Label(self.root, text="Color Settings").grid(row=5,column=0)
+        color_options = ["Light Grey","Light Blue", "Light Pink"]
+        tk.OptionMenu(self.root, self.color_var, *color_options).grid(row=5, column=1)
 
     #function used to input user's turn 
     def play(self, button_id):
